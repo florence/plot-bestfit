4c4ba1e0a169602ec6d115e2e9bc2aaac44cb2e7
Spencer Florence <spencer@florence.io>
2016-03-29 10:04:54 -0500

    fix pkg name

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang setup/infotab
 
 (define version "0.2")
-(define collection 'use-pkg-name)
+(define collection "bestfit")
 (define name "bestfit")
 (define deps '("base" "typed-racket-lib" "plot-lib" "plot-gui-lib" "math-lib"))
 (define build-deps '("racket-doc" "typed-racket-doc" "scribble-lib" "math-doc" "plot-doc"))
